19a60b56bf537c80a5a86a27eb0ac7dc07f2327c
saad1937-berra <saadberra11@gmail.com>
2026-05-30 16:07:26 +0100

    Partie 1 términée

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
 class Main {
 
     public static void main(String[] args) {
-        // TODO
+        Pokemon p1 = new Pokemon(25, "Pikachu", "pokedex_gen1.csv");
+        Pokemon p2 = new Pokemon(6, "Dracaufeu", "pokedex_gen1.csv");
+
+        System.out.println("=== COMBAT ===");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println("==============");
+
+        while (p1.estVivant() && p2.estVivant()) {
+            p1.attaque(p2);
+            System.out.println("---");
+        }
+
+        if (p1.estVivant()) {
+            System.out.println("\nVainqueur : " + p1.getNom() + " !");
+        } else {
+            System.out.println("\nVainqueur : " + p2.getNom() + " !");
+        }
     }
 }
--- Pokemon.java
+++ Pokemon.java
@@ -78,23 +78,81 @@ class Pokemon {
     public void setPv(int pv)      { this.pv = pv; }
 
     public boolean estVivant() {
-        // TODO
-        return false;
+        return pv > 0;
     }
 
     @Override
     public String toString() {
-        // TODO
-        return "";
+        return String.format("%s (n°%d) | Type: %s/%s | PV: %d/%d | ATT: %d | DEF: %d | VIT: %d",
+            nom, numPokedex,
+            Type.getNomType(type1), Type.getNomType(type2),
+            pv, pvMax, att, def, vit);
     }
 
     @Override
     public boolean equals(Object obj) {
-        // TODO
-        return false;
+        if (this == obj) return true;
+        if (!(obj instanceof Pokemon)) return false;
+        Pokemon autre = (Pokemon) obj;
+        return this.numPokedex == autre.numPokedex;
     }
 
+
     public void attaque(Pokemon adversaire) {
-        // TODO
+        // Déterminer qui attaque en premier selon la vitesse
+        Pokemon premier = (this.vit >= adversaire.getVit()) ? this : adversaire;
+        Pokemon second  = (premier == this) ? adversaire : this;
+
+        System.out.println(premier.getNom() + " attaque en premier !");
+
+        // === ATTAQUE DU PREMIER ===
+        double efficacite = Type.getEfficacite(premier.getType1(), second.getType1());
+        if (second.getType2() != Type.SANS) {
+            efficacite *= Type.getEfficacite(premier.getType1(), second.getType2());
+        }
+
+        int degats = (int)((premier.getAtt() - second.getDef()) * efficacite);
+        degats = Math.max(1, degats);
+
+        if (efficacite == 0.0) {
+            System.out.println("Ça n'affecte pas " + second.getNom() + "...");
+            degats = 0;
+        } else if (efficacite > 1.0) {
+            System.out.println("C'est super efficace !");
+        } else if (efficacite < 1.0) {
+            System.out.println("Ce n'est pas très efficace...");
+        }
+
+        second.setPv(second.getPv() - degats);
+        System.out.printf("%s inflige %d dégâts à %s (PV restants : %d/%d)%n",
+            premier.getNom(), degats, second.getNom(), second.getPv(), second.getPvMax());
+
+        // === CONTRE-ATTAQUE DU SECOND (si encore vivant) ===
+        if (second.estVivant()) {
+            System.out.println(second.getNom() + " contre-attaque !");
+
+            double efficacite2 = Type.getEfficacite(second.getType1(), premier.getType1());
+            if (premier.getType2() != Type.SANS) {
+                efficacite2 *= Type.getEfficacite(second.getType1(), premier.getType2());
+            }
+
+            int degats2 = (int)((second.getAtt() - premier.getDef()) * efficacite2);
+            degats2 = Math.max(1, degats2);
+
+            if (efficacite2 == 0.0) {
+                System.out.println("Ça n'affecte pas " + premier.getNom() + "...");
+                degats2 = 0;
+            } else if (efficacite2 > 1.0) {
+                System.out.println("C'est super efficace !");
+            } else if (efficacite2 < 1.0) {
+                System.out.println("Ce n'est pas très efficace...");
+            }
+
+            premier.setPv(premier.getPv() - degats2);
+            System.out.printf("%s inflige %d dégâts à %s (PV restants : %d/%d)%n",
+                second.getNom(), degats2, premier.getNom(), premier.getPv(), premier.getPvMax());
+        } else {
+            System.out.println(second.getNom() + " est K.O. !");
+        }
     }
 }
